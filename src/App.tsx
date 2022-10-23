23b256703d7e8ebf8d33885889f6b5b54cab1cdb
0603anton <zorgzb@gmail.com>
2022-10-23 23:32:48 +0200

    feat: add API fetch and useEffect, useState hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+type PropsType =
+    { userId: number, id: number, title: string, completed: boolean }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState<PropsType[]>([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    }, [])
+
+    const onClickHandler = () => {
+        setTodos([])
+        console.log(todos)
+    }
+    const todosList = todos.map((el) => {
+        return (
+            <li key={el.id}>
+                <span>{el.id} - </span>
+                <span>{el.title}</span>
+                <span>{el.completed}</span>
+                <span>{el.userId}</span>
+            </li>
+        )
+    })
+
+
+    return (
+        <div className="App">
+            <button onClick={onClickHandler}>NEW POSTS</button>
+            <ul>
+                {todosList}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
